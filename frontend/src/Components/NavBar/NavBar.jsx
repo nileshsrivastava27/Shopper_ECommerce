1c40f2c09d3b12e3ecdce8a2a24add7a171aaa27
Nilesh Srivastava <nilesh.sri02@gmail.com>
2024-03-20 01:42:55 +0530

    created navbar

---
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./NavBar.css"
+import logo from '../Assests/logo.png'
+import cart_icon from '../Assests/cart_icon.png'
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+        <div className = 'nav-logo'>
+            <img src = {logo} alt = "Logo"/>
+                <p>SHOPPER</p>
+        </div>
+        <ul className = 'nav-menu'>
+            <li>Shop <hr/></li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+        </ul>
+        <div className='nav-login-cart'>
+            <button>Login</button>
+            <img src={cart_icon} alt='cart'/>
+            <div className='nav-cart-count'>0</div>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
